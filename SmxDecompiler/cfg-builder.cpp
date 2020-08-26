532d0311b6a7bc70ed1721179b38ba454e2b562a
SlidyBat <25954912+SlidyBat@users.noreply.github.com>
2020-08-26 23:19:27 +1000

    Fix CFG generation
    Basic blocks that contained only 1 instruction were handled incorrectly

--- SmxDecompiler/cfg-builder.cpp
+++ SmxDecompiler/cfg-builder.cpp
@@ -13,8 +13,8 @@ ControlFlowGraph CfgBuilder::Build( const cell_t* entry )
 
 	for( const cell_t* leader : leaders_ )
 	{
-		const cell_t* last_instr = NextInstruction( leader );
-		const cell_t* next_leader = last_instr;
+		const cell_t* last_instr = leader;
+		const cell_t* next_leader = NextInstruction( leader );
 		while( next_leader < code_end_ && !IsLeader( next_leader ) )
 		{
 			last_instr = next_leader;
@@ -96,89 +96,74 @@ void CfgBuilder::MarkLeaders( const cell_t* entry )
 	// For now assume end is at end of section
 	code_end_ = smx_->code( smx_->code_size() );
 
-	// Entry point is always a leader
-	AddLeader( entry );
-
-	assert( entry[0] == SMX_OP_PROC );
-
+	// Keep track of how many args this function references
 	int last_arg_offset = 0;
 
-	std::vector<const cell_t*> work_list;
-	work_list.push_back( NextInstruction( entry ) ); // Start after initial PROC
-	while( !work_list.empty() )
+	// Entry point is always a leader
+	AddLeader( entry );
+	assert( entry[0] == SMX_OP_PROC );
+	const cell_t* instr = NextInstruction( entry );
+	while( instr < code_end_ )
 	{
-		bool done = false;
-		const cell_t* instr = work_list.back();
-		work_list.pop_back();
-		while( !done && instr < code_end_ )
+		auto op = (SmxOpcode)instr[0];
+		const cell_t* params = instr + 1;
+		auto& info = SmxInstrInfo::Get( op );
+
+		// Check if any args are referenced
+		for( int param = 0; param < info.num_params; param++ )
 		{
-			auto op = (SmxOpcode)instr[0];
-			const cell_t* params = instr + 1;
-			auto& info = SmxInstrInfo::Get( op );
-
-			// Check if any args are referenced
-			for( size_t param = 0; param < info.num_params; param++ )
+			if( info.params[param] == SmxParam::STACK )
 			{
-				if( info.params[param] == SmxParam::STACK )
-				{
-					cell_t offset = params[param];
-					last_arg_offset = std::max( last_arg_offset, offset );
-				}
+				cell_t offset = params[param];
+				last_arg_offset = std::max( last_arg_offset, offset );
 			}
-
-			switch( op )
-			{
-				case SMX_OP_JUMP:
-				{
-					cell_t* target = smx_->code( instr[1] );
-					work_list.push_back( target );
-					AddLeader( target );
-					done = true;
-					break;
-				}
-				case SMX_OP_JEQ:
-				case SMX_OP_JNEQ:
-				case SMX_OP_JZER:
-				case SMX_OP_JNZ:
-				case SMX_OP_JSGRTR:
-				case SMX_OP_JSGEQ:
-				case SMX_OP_JSLESS:
-				case SMX_OP_JSLEQ:
-				{
-					cell_t* target = smx_->code( instr[1] );
-					work_list.push_back( target );
-					AddLeader( target );
-					AddLeader( NextInstruction( instr ) );
-					break;
-				}
-				case SMX_OP_SWITCH:
-				{
-					cell_t* casetbl = smx_->code( instr[1] );
-					cell_t ncases = casetbl[1];
-					cell_t* def = smx_->code( casetbl[2] );
-					work_list.push_back( def );
-					AddLeader( def );
-					for( cell_t i = 0; i < ncases; i++ )
-					{
-						cell_t* target = smx_->code( casetbl[2 + i * 2 + 1] );
-						work_list.push_back( target );
-						AddLeader( target );
-					}
-					done = true;
-					break;
-				}
-				case SMX_OP_ENDPROC:
-				case SMX_OP_PROC:
-				{
-					// Found end of function, update it
-					code_end_ = instr;
-					done = true;
-					break;
-				}
-			}
-
-			instr = NextInstruction( instr );
 		}
+
+		switch( op )
+		{
+			case SMX_OP_JUMP:
+			{
+				cell_t* target = smx_->code( instr[1] );
+				AddLeader( target );
+				break;
+			}
+			case SMX_OP_JEQ:
+			case SMX_OP_JNEQ:
+			case SMX_OP_JZER:
+			case SMX_OP_JNZ:
+			case SMX_OP_JSGRTR:
+			case SMX_OP_JSGEQ:
+			case SMX_OP_JSLESS:
+			case SMX_OP_JSLEQ:
+			{
+				cell_t* target = smx_->code( instr[1] );
+				AddLeader( target );
+				AddLeader( NextInstruction( instr ) );
+				break;
+			}
+			case SMX_OP_SWITCH:
+			{
+				cell_t* casetbl = smx_->code( instr[1] );
+				cell_t ncases = casetbl[1];
+				cell_t* def = smx_->code( casetbl[2] );
+				AddLeader( def );
+				for( cell_t i = 0; i < ncases; i++ )
+				{
+					cell_t* target = smx_->code( casetbl[2 + i * 2 + 1] );
+					AddLeader( target );
+				}
+				break;
+			}
+			case SMX_OP_ENDPROC:
+			case SMX_OP_PROC:
+			{
+				// Found end of function, update it
+				code_end_ = instr;
+				break;
+			}
+		}
+
+		instr = NextInstruction( instr );
 	}
 
 	if( last_arg_offset >= 12 )
